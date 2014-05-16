f3b6744c95cf6ea8e8f028ae9e098f206e89cdea
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-05-16 14:07:29 +0200

    Parse framework added

--- SecretSpice/SecretSpice/Application/SSPAppDelegate.m
+++ SecretSpice/SecretSpice/Application/SSPAppDelegate.m
@@ -12,6 +12,9 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [Parse setApplicationId:@"BlU8Ak59EUAYRz9TxZ3gz84n9ZslKZl2W6TtuXbf"
+                  clientKey:@"pR7It3eiKkpegGeTnPYs4tvmi2qu0h1ecUWBIZ5r"];
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     return YES;
 }
 
---
+++ SecretSpice/SecretSpice/Supporting/FBMissingSymbols.m
@@ -0,0 +1,31 @@
+//
+//  FBMissingSymbols.m
+//  SecretSpice
+//
+//  Created by Renzo Crisóstomo on 16/05/14.
+//  Copyright (c) 2014 UIKonf. All rights reserved.
+//
+
+NSString *FBTokenInformationExpirationDateKey = @"";
+NSString *FBTokenInformationTokenKey = @"";
+NSString *FBTokenInformationUserFBIDKey = @"";
+@interface FBAppCall:NSObject
+@end
+@implementation FBAppCall
+@end
+@interface FBRequest:NSObject
+@end
+@implementation FBRequest
+@end
+@interface FBSession:NSObject
+@end
+@implementation FBSession
+@end
+@interface FBSessionTokenCaching:NSObject
+@end
+@implementation FBSessionTokenCaching
+@end
+@interface FBSessionTokenCachingStrategy:NSObject
+@end
+@implementation FBSessionTokenCachingStrategy
+@end
