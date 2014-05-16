1f8f9974e9806a144b4029c4a2602b2f41e6b870
Renzo Crisóstomo <ruenzuo@gmail.com>
2014-05-16 11:51:10 +0200

    Project setup

---
+++ SecretSpice/SecretSpice/Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SecretSpice
+//
+//  Created by Renzo Crisóstomo on 16/05/14.
+//  Copyright (c) 2014 UIKonf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SecretSpice/SecretSpice/Supporting/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SecretSpice
+//
+//  Created by Renzo Crisóstomo on 16/05/14.
+//  Copyright (c) 2014 UIKonf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
